8277b19db3c86172bd5468e8559e93f581ceb249
Diego Sousa <diegodev.geomais@gmail.com>
2024-05-06 22:35:02 -0300

    fix: adjust calculations

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -10,6 +10,7 @@ public class Turret : MonoBehaviour
     [SerializeField] GameObject turretHead;
     [SerializeField] GameObject turretBase;
     [SerializeField] LayerMask layerMaskTarget;
+    [SerializeField] Camera cam;
 
     void Update() {
         Aim();
@@ -17,19 +18,21 @@ public class Turret : MonoBehaviour
 
     void Aim() {
         Vector3 mouseScreenPos = Input.mousePosition; // Position with x and z relative to the camera view
-        Vector3 mouseScreenPosWithDistance = new(mouseScreenPos.x, mouseScreenPos.y, Camera.main.farClipPlane); // Position with the distance set to the camera far
-        Vector3 mouseWorldPoint = Camera.main.ScreenToWorldPoint(mouseScreenPosWithDistance); // Final world x,y and z coords
+        Vector3 mouseScreenPosWithDistance = new(mouseScreenPos.x, mouseScreenPos.y, cam.farClipPlane); // Position with the distance set to the camera far
+        Vector3 mouseWorldPoint = cam.ScreenToWorldPoint(mouseScreenPosWithDistance); // Final world x,y and z coords
         
-        Debug.DrawRay(transform.position, mouseWorldPoint, Color.green);
+        Debug.DrawRay(cam.transform.position, mouseWorldPoint, Color.green);
 
-        if (Physics.Raycast(transform.position, mouseWorldPoint, out RaycastHit hit, Camera.main.farClipPlane, layerMaskTarget)) {
-            Vector3 resultedEulerTarget = transform.position - hit.point;
+        if (Physics.Raycast(cam.transform.position, mouseWorldPoint, out RaycastHit hit, cam.farClipPlane, layerMaskTarget)) {
+            Vector3 resultedEulerTarget = turretBase.transform.position - hit.point;
 
-            float verticalAngle = Mathf.Atan2(resultedEulerTarget.y, resultedEulerTarget.z) * Mathf.Rad2Deg;
-            float horizontalAngle = Mathf.Atan2(resultedEulerTarget.x, resultedEulerTarget.z) * Mathf.Rad2Deg;
+            float verticalAngle = Mathf.Asin(resultedEulerTarget.y / resultedEulerTarget.magnitude) * Mathf.Rad2Deg;
+            float horizontalAngle = Mathf.Atan2(-resultedEulerTarget.x, -resultedEulerTarget.z) * Mathf.Rad2Deg;
 
-            Quaternion headRotation = Quaternion.Euler(verticalAngle, horizontalAngle, turretHead.transform.position.z);
-            Quaternion baseRotation = Quaternion.Euler(turretBase.transform.position.x, horizontalAngle, turretBase.transform.position.z);
+            if (horizontalAngle < 0f) horizontalAngle += 360f;
+
+            Quaternion headRotation = Quaternion.Euler(verticalAngle, horizontalAngle, 0f);
+            Quaternion baseRotation = Quaternion.Euler(0f, horizontalAngle, 0f);
 
             turretHead.transform.rotation = headRotation;
             turretBase.transform.rotation = baseRotation;
