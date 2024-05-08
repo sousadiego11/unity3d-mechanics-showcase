28fa6c54f5c23b7991894cc784d94a1e0e94c473
Diego Sousa <diegodev.geomais@gmail.com>
2024-05-08 19:01:31 -0300

    feat: add new fx and missile hit fx

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Missile : MonoBehaviour
 {
     Vector3 targetPositionCache;
+    [SerializeField] GameObject effectToInstantiate;
 
     void Start() {
         TargetPointer targetPointer = GameObject.FindGameObjectWithTag("Pointer").GetComponent<TargetPointer>();
@@ -15,5 +16,12 @@ public class Missile : MonoBehaviour
         Debug.DrawRay(transform.position, targetPositionCache - transform.position, Color.magenta);
         Vector3 direction = (targetPositionCache - transform.position).normalized;
         transform.Translate(5f * Time.deltaTime * direction, Space.World);
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag("Hittable")) {
+            Instantiate(effectToInstantiate, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
     }
 }
