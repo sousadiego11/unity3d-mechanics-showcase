fb683442e8f62af34119cc2cf2273bef0efd9332
Diego Sousa <diegodev.geomais@gmail.com>
2024-05-09 13:09:14 -0300

    refactor: change audio and add misile rotation

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -8,6 +8,8 @@ public class Missile : MonoBehaviour
     [SerializeField] GameObject effectToInstantiate;
     [SerializeField] float explosionPower;
     [SerializeField] float explosionRadius;
+    [SerializeField] float travelVelocity;
+    [SerializeField] float spinVelocity;
 
     void Start() {
         TargetPointer targetPointer = GameObject.FindGameObjectWithTag("Pointer").GetComponent<TargetPointer>();
@@ -17,7 +19,8 @@ public class Missile : MonoBehaviour
     void Update() {
         Debug.DrawRay(transform.position, targetPositionCache - transform.position, Color.magenta);
         Vector3 direction = (targetPositionCache - transform.position).normalized;
-        transform.Translate(5f * Time.deltaTime * direction, Space.World);
+        transform.Rotate(spinVelocity * Time.deltaTime * Vector3.forward, Space.Self);
+        transform.Translate(travelVelocity * Time.deltaTime * direction, Space.World);
     }
 
     void OnCollisionEnter(Collision other) {
@@ -25,7 +28,7 @@ public class Missile : MonoBehaviour
             Instantiate(effectToInstantiate, transform.position, Quaternion.identity);
             foreach (Collider hitCollider in Physics.OverlapSphere(transform.position, 5)) {
                 bool foundRb = hitCollider.TryGetComponent(out Rigidbody rb);
-                if (foundRb) rb.AddExplosionForce(explosionPower, transform.position, explosionRadius, 3.0F);
+                if (foundRb) rb.AddExplosionForce(explosionPower, transform.position, explosionRadius);
             }
             Destroy(gameObject);
         }
