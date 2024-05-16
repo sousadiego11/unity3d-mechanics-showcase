065b0cbb38098209f5a6998b33317236b7b9fa7e
Diego Sousa <diegodev.geomais@gmail.com>
2024-05-15 22:30:24 -0300

    feat: add initial ui structure

---
+++ Assets/Scripts/MechanicInspector.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechanicInspector : MonoBehaviour
+{
+    void Update() {
+        transform.Rotate(Vector3.up * Time.deltaTime * 30);
+    }
+}
